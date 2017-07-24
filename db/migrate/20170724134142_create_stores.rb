f69c6b9c11867f2bdc4d5038f11ab9a786ec5de6
newtonkiragu <karanunewton4@gmail.com>
2017-07-24 16:42:53 +0300

    created a migration for stores

---
+++ db/migrate/20170724134142_create_stores.rb
@@ -0,0 +1,9 @@
+class CreateStores < ActiveRecord::Migration[5.1]
+  d  def change
+  create_table(:stores) do |t|
+    t.column(:store_name, :varchar)
+    t.column(:location, :varchar)
+    t.timestamps()
+  end
+end
+end
